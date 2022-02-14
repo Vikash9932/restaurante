3ceb091a59d9cd27e060cda4e07a190976bfe401
Vikash Kumar <vikash9932@gmail.com>
2022-02-14 17:02:07 +0530

    feat: Chef implementation

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding" id="chef">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+      <div className="app__chef-content">
+        <div className="app_chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eaque,
+          obcaecati autem nemo libero modi.
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef &amp; Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
